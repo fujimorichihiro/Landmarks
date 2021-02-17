f9ae6b0693ba647454f9e75ca7aab74c0f8886d5
fujimorichihiro <fujimori.drum@gmail.com>
2021-02-17 11:55:05 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 藤森千裕 on 2021/02/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
