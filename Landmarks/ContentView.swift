0666974fc7f25d0b5782e9cb30698607ce231176
fujimorichihiro <fujimori.drum@gmail.com>
2021-02-17 12:17:40 +0900

    Customize the text

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, SwiftUI!")
+            .font(.title)
+            .foregroundColor(.green)
             .padding()
     }
 }
